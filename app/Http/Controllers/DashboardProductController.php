eedfefb5935d4a378d00859b61d8cbdf061569f5
novaari2 <novaari20@gmail.com>
2021-08-17 22:17:26 +0700

    first commit, next start from categories

---
+++ app/Http/Controllers/DashboardProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardProductController extends Controller
+{
+    public function index()
+    {
+        return view('pages.dashboard-products');
+    }
+
+    public function details()
+    {
+        return view('pages.dashboard-products-detail');
+    }
+
+    public function add()
+    {
+        return view('pages.dashboard-products-add');
+    }
+}
---
+++ database/migrations/2021_08_17_071734_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('users_id');
+            $table->double('insurance_price');
+            $table->double('shipping_price');
+            $table->double('total_price');
+            $table->string('transaction_status'); //unpaid/pending/success/failed
+            $table->string('resi');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
